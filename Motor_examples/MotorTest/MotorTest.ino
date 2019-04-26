c3649c32109c8b4ea0716452c0be19c1bdbf43a6
hafpaf <haf@hafnium.me>
2019-04-26 10:53:16 +0200

    DC-motor tests

---
+++ Motor_examples/MotorTest/MotorTest.ino
@@ -0,0 +1,43 @@
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
+
+#include <AFMotor.h>
+
+//Motor M1
+AF_DCMotor motor(1);
+
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+
+  // turn on motor
+  motor.setSpeed(200);
+ 
+  motor.run(RELEASE);
+}
+
+void loop() {
+  uint8_t i;
+  
+  Serial.print("tick");
+  
+  motor.run(FORWARD);
+  for (i=0; i<255; i++) {
+    motor.setSpeed(i);
+    delay(10);
+  }
+ 
+  Serial.print("tock");
+
+  motor.run(BACKWARD);
+  for (i=0; i<255; i++) {
+    motor.setSpeed(i); 
+    delay(10);
+ }
+
+  Serial.print("tech");
+  motor.run(RELEASE);
+  delay(1000);
+}
